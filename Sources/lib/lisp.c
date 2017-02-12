e4d4fee114a6020c639db2062872aa73dd6322ce
Murai Takashi <tmurai01@gmail.com>
2017-02-12 20:27:59 +0900

    More remove "Learning status display menu" .

--- Sources/lib/lisp.c
+++ Sources/lib/lisp.c
@@ -4615,7 +4615,6 @@ static struct cannafndefs cannafns[] = {
   {S_FN_DisconnectServer	,CANNA_FN_DisconnectServer},
   {S_FN_ChangeServerMode	,CANNA_FN_ChangeServerMode},
   {S_FN_ShowServer		,CANNA_FN_ShowServer},
-  {S_FN_ShowGakushu		,CANNA_FN_ShowGakushu},
   {S_FN_ShowPhonogramFile	,CANNA_FN_ShowPhonogramFile},
   {S_FN_ShowCannaFile		,CANNA_FN_ShowCannaFile},
   {S_FN_PageUp			,CANNA_FN_PageUp},
